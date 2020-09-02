c2c0aa9f8b29c6680d08de20bb93677a97cfdb76
Igor Petrenko <sabotag1985@gmail.com>
2020-09-02 11:18:15 +0300

    Revert "test to add a new group from AddressBook App"
    This reverts commit a60a62d7

--- conftest.py
+++ conftest.py
@@ -1,5 +1,4 @@
 import pytest
-
 from fixture.application import Application
 
 
---
+++ test/test_add_groups.py
@@ -0,0 +1,6 @@
+def test_add_group(app):
+    old_lst = app.groups.get_group_list()
+    app.groups.add_new_group("my group")
+    new_lst = app.groups.get_group_list()
+    old_lst.append("my group")
+    assert sorted(old_lst) == sorted(new_lst)
