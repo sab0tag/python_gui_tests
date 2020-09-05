35422a4937c099055645b4998bf2c0c3f5066d9c
Igor Petrenko <sabotag1985@gmail.com>
2020-09-05 13:17:54 +0300

    Create groups generation to excel file as a separate func add test to load data from the file paramerize the test

--- test/test_add_group_xlsx.py
+++ test/test_add_group_xlsx.py
@@ -1,5 +1,7 @@
+import pytest
 from comtypes.client import CreateObject
 import os
+
 
 # to save xlsx file inside of the project folder set project path as default
 project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -9,9 +11,15 @@ file_to_save = os.path.join(project_dir, "data/groups.xlsx")
 xl = CreateObject("Excel.Application")
 # make it visible
 xl.Visible = 1
-wb = xl.Workbooks.Add()
-# add data
-for i in range(100):
-    xl.Range["A%s" % (i + 1)].Value[()] = "group %s" % str(i+1)
-wb.SaveAs(file_to_save)
+wb = xl.Workbooks.Open(file_to_save)
+ws = wb.Worksheets(1)
+data_list = []
+for each_row in range(1, 99):
+    dynamic_data = ws.Cells[each_row, 1].Value[()]
+    data_list.append(dynamic_data)
 xl.Quit()
+
+
+@pytest.mark.parametrize("group", data_list, ids=[repr(x) for x in data_list])
+def test_load_groups(group):
+    pass
